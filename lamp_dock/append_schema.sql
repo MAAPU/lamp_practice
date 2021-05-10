7b582b84d7713788bc3b5f33e9f9aa3c6f91d78a
masaki <makku.aihara@gmail.com>
2021-05-10 23:48:33 +0900

    修正済です

--- lamp_dock/append_schema.sql
+++ lamp_dock/append_schema.sql
@@ -1,14 +1,14 @@
 CREATE TABLE'order_history'(
-    'order_id' int(11) INT AUTO_INCREMENT not null,
-    'user_id' int(11) not null,
-    'create_datetime' datetime not null,
-    primary key(order_id),   
+     `order_id` int(11)  AUTO_INCREMENT not null,
+     `user_id` int(11) not null,
+     `create_datetime` datetime not null,
+    primary key(order_id)   
 )
 
 CREATE TABLE'order_details'(
-    `item_id` int(11) INT AUTO_INCREMENT NOT NULL,
-    `name` varchar(100) NOT NULL,
+    `item_id` int(11)  AUTO_INCREMENT NOT NULL,
+    `order_id` int(11)  AUTO_INCREMENT not null,
     `price` int(11) NOT NULL,
     `amount` int(11) DEFAULT 0,
-    primary key(item_id),
+    primary key(item_id, order_id)
 )
